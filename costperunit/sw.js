d3b86d413ffa4f05a2a45bd8e831a16d7ead93fd
Taly <vitalik7tv@yandex.ru>
2020-08-06 13:25:06 +0300

    Update

--- costperunit/sw.js
+++ costperunit/sw.js
@@ -1,20 +1,52 @@
+let CACHE_KEY = 'costperunit';
+
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open('costperunit').then(function(cache) {
+    caches.open(CACHE_KEY).then(function(cache) {
       return cache.addAll([
-        '/costperunit/',
-        '/costperunit/index.html',
-        '/costperunit/style.css'
+        '/costperunit/'
       ]);
     })
   );
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+  console.log(`The service worker is serving the asset ${e.request.url}`);
+
+  e.respondWith(fromCache(e.request));
+
+  e.waitUntil(
+    update(e.request)
+      .then(refresh)
   );
 });
+
+function fromCache(request) {
+  return caches.open(CACHE_KEY).then(function (cache) {
+    return cache.match(request);
+  });
+}
+
+function update(request) {
+  return caches.open(CACHE_KEY).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response.clone()).then(function () {
+        return response;
+      });
+    });
+  });
+}
+
+function refresh(response) {
+  return self.clients.matchAll().then(function (clients) {
+    clients.forEach(function (client) {
+      var message = {
+        type: 'refresh',
+        url: response.url,
+        eTag: response.headers.get('ETag')
+      }
+
+      client.postMessage(JSON.stringify(message));
+    });
+  });
+}
